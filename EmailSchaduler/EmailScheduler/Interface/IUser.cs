2893f75e7c0aac301f04342179670837a622da5e
hassnain <hassnainiqbal@gmail.com>
2019-01-25 10:24:01 +0800

    no message

---
+++ EmailSchaduler/EmailScheduler/Interface/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailSchedulerData;
+
+namespace EmailScheduler.Interface
+{
+    interface IUser
+    {
+        bool UpdateUserState();
+
+        bool UpdateEmailSentDate(int userId);
+
+        List<UserProfile> GetUsers();
+    }
+}
---
+++ EmailSchaduler/EmailScheduler/User.cs
@@ -0,0 +1,36 @@
+﻿using EmailScheduler.Interface;
+using EmailSchedulerData;
+using System;
+using System.Collections.Generic;
+
+namespace EmailScheduler
+{
+    class User : IUser
+    {
+        private readonly EmailSchedulerData.emailSenderEntities context;
+
+        public User()
+        {
+            context = new EmailSchedulerData.emailSenderEntities();
+        }
+
+        public List<UserProfile> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEmailSentDate(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUserState()
+        {
+            using (var context = new EmailSchedulerData.emailSenderEntities())
+            {
+                //var result = (from p in context.UserProfiles select p);
+            }
+            return true;
+        }
+    }
+}
